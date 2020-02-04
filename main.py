8b9d614acdfa4e200c731fb189075850e42a3a09
Nick <scifi6546@protonmail.com>
2020-02-04 13:32:50 -0900

    saved data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from keras.utils import to_categorical
+import random
 
 import numpy as np
 import obspy as ob
@@ -24,7 +25,7 @@ def pick_emptytime(events,time_window,station):
 
 
 arrival_data=[]
-num_samples = 6000;
+num_samples = 15000;
 
 
 
@@ -48,14 +49,18 @@ for eq_file in files:
             break
         for event in arrival_data:
             if event["station"]==data[0].stats["station"]:
+                rand_num = random.uniform(0,time_window/1.3);
+                time_start = event["time"]-time_window+rand_num
+                time_end = event["time"]+time_window+rand_num
                 arrival = data.slice(ob.UTCDateTime(event["time"]-time_window)
                         ,ob.UTCDateTime(event["time"]+time_window))
-                phase_array=np.array([0])
+                phase_array=np.array([0,0])
+                normalized_time = (rand_num)/(2*time_window)+0.5
                     
                 if(event["phase"]=='S'):
-                    phase_array=np.array([1])
+                    phase_array=np.array([1,normalized_time])
                 if(event["phase"]=='P'):
-                    phase_array=np.array([2])
+                    phase_array=np.array([2,normalized_time])
 
                 try:
                     print(arrival[0].data.shape)
@@ -73,16 +78,12 @@ for eq_file in files:
                         print("phase_train empty")
                     else:
                         phase_train=np.vstack([phase_train,phase_array])
-                else:
-                    print("shape wrong!")
-                    print(arrival[0].data.shape)
-
                 break;
         empty_times = pick_emptytime(arrival_data,time_window*2,data[0].stats["station"])
         for time in empty_times:
             arrival = data.slice(ob.UTCDateTime(time-time_window)
                 ,ob.UTCDateTime(time+time_window))
-            phase_array=np.array([0])
+            phase_array=np.array([0,0.0])
             try:
                 print(arrival[0].data.shape)
             except:
@@ -108,6 +109,11 @@ for eq_file in files:
 
         print(str((float(i)/float(num_samples))*100.0)+"%")
         i+=1
+phase_train=np.array(phase_train)
+print("phase_train shape")
+print(phase_train.shape)
+np.save("phase_array.npy",phase_train)
+np.save("wf_array.npy",wf_train)
 model = model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(units = 64,input_shape=(wf_train[0].shape)),
   tf.keras.layers.Dense(128, activation='relu'),
@@ -115,13 +121,13 @@ model = model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(activation='softmax',units=3)
+  tf.keras.layers.Dense(activation='softmax',units=2)
 ])
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-
+print(model.summary())
 print(wf_train.shape)
 print(phase_train)
-model.fit(wf_train,to_categorical(phase_train),epochs=10)
-model.evaluate(wf_train,to_catagorical(phase_train))
+model.fit(wf_train,(phase_train),epochs=10)
+model.evaluate(wf_train,phase_train)
